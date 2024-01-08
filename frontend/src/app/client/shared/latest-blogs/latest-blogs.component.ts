622f2dff145100a3f331c47aeec39922281bc6ce
paulatanu1 <atanupaul76@gmail.com>
2024-01-09 01:29:29 +0530

    blog bug fix and ddetails page added

--- frontend/src/app/client/shared/latest-blogs/latest-blogs.component.ts
+++ frontend/src/app/client/shared/latest-blogs/latest-blogs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CarouselConfig } from 'ngx-bootstrap/carousel';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -23,6 +24,88 @@ export class LatestBlogsComponent implements OnInit {
   noWrap = false;
   private innerWidth: number | undefined;
   private mobileBreakpoint = 767;
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 3,
+      },
+    },
+    nav: true,
+  };
+  blogs: {
+    blogImg: string;
+    blogText: string;
+    blogHeading: string;
+    author: string;
+    comments: number;
+    month: string;
+    date: number;
+  }[] = [
+    {
+      blogImg: 'assets/images/blog/blog_1.jpg',
+      blogText:
+        'Lab-grown diamonds have emerged as a sustainable and ethical alternative to natural diamonds.[....]',
+      blogHeading:
+        'Lab Grown Diamonds vs. Natural Diamonds: Exploring the Differences',
+      author: 'Anderson',
+      comments: 3,
+      month: 'Aug',
+      date: 15,
+    },
+
+    {
+      blogImg: 'assets/images/blog/Blog_2.jpg',
+      blogText:
+        'Lab-grown diamonds have opened up a world of possibilities when it comes to diamond shapes. [....]',
+      blogHeading:
+        'Unveiling the Brilliance: Unique Shapes in Lab-Grown Diamonds',
+      author: 'Anderson',
+      comments: 3,
+      month: 'Aug',
+      date: 15,
+    },
+
+    {
+      blogImg: 'assets/images/blog/Blog_3.jpg',
+      blogText:
+        'In recent years, there has been a remarkable surge in the demand for lab-grown diamonds. As a leading manufacturer and exporter[....]',
+      blogHeading:
+        'The Ever-Growing Sparkle: The Increasing Demand for Lab-Grown Diamonds',
+      author: 'Anderson',
+      comments: 3,
+      month: 'Aug',
+      date: 15,
+    },
+
+    {
+      blogImg: 'assets/images/blog/Blog_2.jpg',
+      blogText:
+        'Lab-grown diamonds have opened up a world of possibilities when it comes to diamond shapes.[....]',
+      blogHeading:
+        'Unveiling the Brilliance: Unique Shapes in Lab-Grown Diamonds',
+      author: 'Anderson',
+      comments: 3,
+      month: 'Aug',
+      date: 15,
+    },
+  ];
   constructor() {}
   ngOnInit(): void {
     this.adjustsItemsPerSlide();
